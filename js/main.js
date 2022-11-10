f2e7ab71de24bd4df76f34a712cef9e4cc24ddf9
zinorella505 <mcgeecharles.ba@gmail.com>
2022-11-10 04:42:38 -0500

    first push

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+
+let body = document.body
+let toggle = document.querySelector('.toggle')
+let icon = document.querySelector('.fa-moon')
+let iconB = document.querySelector('dark')
+
+toggle.onclick = () =>{
+    body.classList.toggle("dark")
+    if(body.classList.contains('dark')){
+        icon.classList.replace('fa-moon', 'fa-sun');
+    }else{
+        icon.classList.replace('fa-sun', 'fa-moon')
+    }
+}
+
+// toggle.onclick = () =>{
+//     body.classList.toggle("dark")
+//     if(body.classList.contains('dark')){
+//         icon.classList.replace('fa-moon', 'fa-sun');
+//     }else{
+//         icon.classList.replace('fa-sun', 'fa-moon');
+//     }
+// }
+
+
+// let body = document.body
+// let toggle = document.querySelector('.toggle')
+// let icon = document.querySelector('fa-moon')
+
+// function checkIcon(){
+//     if(body.classList.contains('dark')){
+//         icon.classList.add('fa-moon')
+//         icon.classList.remove('fa-sun')
+//     }else{
+//         icon.classList.add('fa-sun')
+//         icon.classList.remove('fa-moon')
+//     }
+// }
+
+// checkIcon()
+
+// toggle.onclick = () =>{
+//     body.classList.toggle("dark")
+//     setTimeout(()=>{
+//         checkIcon()
+//     },100)
+// }
